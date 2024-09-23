499046c69e75ae6a0b4e6434d0580dff3476af35
aleksandrekas <154304980+aleksandrekas@users.noreply.github.com>
2024-09-23 22:18:32 +0400

    Update App.jsx

--- quote-machine/src/App.jsx
+++ quote-machine/src/App.jsx
@@ -1,5 +1,4 @@
 import QuoteBox from "./components/QuoteBox"
-import { useEffect, useState } from "react";
 
 function getRandomColor() {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -9,13 +8,12 @@ function getRandomColor() {
 
 
 function App() {
-  const [color,setColor] = useState("purple")
 
 
 
   return (
-    <div style={{background:color}} className="container">
-      <QuoteBox color={color} />
+    <div style={{background:"purple"}} className="container">
+      <QuoteBox color={"purple"} />
     </div>
   )
 }
